9e50d26c2e9d14630abb773c30e292c8c9938651
Rockwell <ajrockwell@email.neit.edu>
2023-10-10 14:12:37 -0400

    site

---
+++ site/index.php
@@ -0,0 +1,17 @@
+<h1>PHP and My SQL - Andrew Rockwell</h1>
+
+<h2>Assignments</h2>
+
+<!--taken from class_web_site and edited to my folder names-->
+    <ul>
+        <li><a href="../w1/index.php">Week 1</a></li>
+        <li><a href="../w2/index.php">Week 2</a></li>
+        <li><a href="../w3/index.php">Week 3</a></li>
+        <li><a href="../w4/index.php">Week 4</a></li>
+        <li><a href="../w5/index.php">Week 5</a></li>
+        <li><a href="../w6/index.php">Week 6</a></li>
+        <li><a href="../w7/index.php">Week 7</a></li>
+        <li><a href="../w8/index.php">Week 8</a></li>
+        <li><a href="../w9/index.php">Week 9</a></li>
+        <li><a href="../w10/index.php">Week 10</a></li>
+    </ul>
---
+++ w1/index.php
@@ -0,0 +1,7 @@
+<ul>
+        <li><a href="Task-C/index.php">Week 1</a></li>
+        <li><a href="Task-D/index.php">Week 2</a></li>
+        <li><a href="Task-E/index.php">Week 3</a></li>
+        <li><a href="Task-F/index.php">Week 4</a></li>
+        <li><a href="Task-G/FizzBuzz.php">Week 5</a></li>
+    </ul>
