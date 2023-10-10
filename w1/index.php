65d2d8d907b31f30755ee2aa48d893e1815930a3
Rockwell <ajrockwell@email.neit.edu>
2023-10-10 14:19:36 -0400

    site

--- w1/index.php
+++ w1/index.php
@@ -1,7 +1,7 @@
 <ul>
-        <li><a href="Task-C/index.php">Week 1</a></li>
-        <li><a href="Task-D/index.php">Week 2</a></li>
-        <li><a href="Task-E/index.php">Week 3</a></li>
-        <li><a href="Task-F/index.php">Week 4</a></li>
-        <li><a href="Task-G/FizzBuzz.php">Week 5</a></li>
+        <li><a href="Task-C/index.php">Task-C</a></li>
+        <li><a href="Task-D/index.php">Task-D</a></li>
+        <li><a href="Task-E/index.php">Task-E</a></li>
+        <li><a href="Task-F/index.php">Task-F</a></li>
+        <li><a href="Task-G/FizzBuzz.php">Task-G</a></li>
     </ul>
