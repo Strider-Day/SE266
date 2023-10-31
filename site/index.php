8e1f97a34e45af6eae0d20955de315e968b82bd0
Rockwell <ajrockwell@email.neit.edu>
2023-10-31 13:47:58 -0400

    w3 hw

--- site/index.php
+++ site/index.php
@@ -6,7 +6,7 @@
     <ul>
         <li><a href="../w1/index.php">Week 1</a></li>
         <li><a href="../w2/index.php">Week 2</a></li>
-        <li><a href="../w3/index.php">Week 3</a></li>
+        <li><a href="../w3/atm_starter.php">Week 3</a></li>
         <li><a href="../w4/index.php">Week 4</a></li>
         <li><a href="../w5/index.php">Week 5</a></li>
         <li><a href="../w6/index.php">Week 6</a></li>
