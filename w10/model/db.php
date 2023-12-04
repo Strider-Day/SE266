5686f75781c1ba3868b02f5fe87ba38d5a1a43aa
Rockwell <ajrockwell@email.neit.edu>
2023-12-04 12:42:40 -0500

    final prep

---
+++ w10/model/db.php
@@ -0,0 +1,16 @@
+<?php
+
+$ini = parse_ini_file( __DIR__ . '/dbconfig.ini');
+
+// var_dump($ini);
+// exit;
+
+$db = new PDO(
+                "mysql:host=" . $ini['servername'] . 
+                ";port=" . $ini['port'] . 
+                ";dbname=" . $ini['dbname'], 
+                $ini['username'], 
+                $ini['password']);
+
+
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
